a7177d357573b352a5747eee8c275131d5b42360
Michael Brannen <MBrannen@student.neumont.edu>
2017-02-25 20:24:01 -0700

    spaghetti

---
+++ src/edu/neumont/csc150/destitute/game/model/Buyable.java
@@ -0,0 +1,40 @@
+package edu.neumont.csc150.destitute.game.model;
+
+public abstract class Buyable {
+	private String name;
+	private int health;
+	private int markCost;
+	private int lumberCost;
+	private int stoneCost;
+	
+	public int getStoneCost() {
+		return stoneCost;
+	}
+	public void setStoneCost(int stoneCost) {
+		this.stoneCost = stoneCost;
+	}
+	public int getLumberCost() {
+		return lumberCost;
+	}
+	public void setLumberCost(int lumberCost) {
+		this.lumberCost = lumberCost;
+	}
+	public int getMarkCost() {
+		return markCost;
+	}
+	public void setMarkCost(int markCost) {
+		this.markCost = markCost;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
