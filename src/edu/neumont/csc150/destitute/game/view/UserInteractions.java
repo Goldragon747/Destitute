34ebc4a36841d61446801e9c1cf53c4cc0b73853
Gabe Barber <GBarber@student.neumont.edu>
2017-02-27 14:39:55 -0700

    fdsa

--- src/edu/neumont/csc150/destitute/game/view/UserInteractions.java
+++ src/edu/neumont/csc150/destitute/game/view/UserInteractions.java
@@ -1,8 +1,13 @@
 package edu.neumont.csc150.destitute.game.view;
 
+import java.util.Random;
+
 public class UserInteractions {
+	private Random gen = new Random();
 	private GUI gui;
 	public UserInteractions(GUI gui) {
 		this.gui = gui;
+		gen.nextInt(10);
+		
 	}
 }
