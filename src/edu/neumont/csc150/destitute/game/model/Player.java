b1a63ebdd9f2fb75a8301d0ff05f928191f8bb2d
Gabe Barber <GBarber@student.neumont.edu>
2017-03-01 11:22:10 -0700

    fdsa

--- src/edu/neumont/csc150/destitute/game/model/Player.java
+++ src/edu/neumont/csc150/destitute/game/model/Player.java
@@ -2,4 +2,36 @@ package edu.neumont.csc150.destitute.game.model;
 
 public class Player {
 private int marks, stone, lumber, horses;
+
+public int getMarks() {
+	return marks;
+}
+
+public void setMarks(int marks) {
+	this.marks = marks;
+}
+
+public int getStone() {
+	return stone;
+}
+
+public void setStone(int stone) {
+	this.stone = stone;
+}
+
+public int getLumber() {
+	return lumber;
+}
+
+public void setLumber(int lumber) {
+	this.lumber = lumber;
+}
+
+public int getHorses() {
+	return horses;
+}
+
+public void setHorses(int horses) {
+	this.horses = horses;
+}
 }
