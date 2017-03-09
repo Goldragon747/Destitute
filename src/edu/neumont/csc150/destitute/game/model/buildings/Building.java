708fdf33f4a08c4780e01b17a00f062bde2ea872
Gabe Barber <GBarber@student.neumont.edu>
2017-03-08 21:18:53 -0700

    fdas

--- src/edu/neumont/csc150/destitute/game/model/buildings/Building.java
+++ src/edu/neumont/csc150/destitute/game/model/buildings/Building.java
@@ -3,5 +3,5 @@ package edu.neumont.csc150.destitute.game.model.buildings;
 import edu.neumont.csc150.destitute.game.model.Buyable;
 
 public abstract class Building extends Buyable{
-	//TODO Stretch: repairing?
+	
 }
